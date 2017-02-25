caec6e05348beadc96f2812825fb81d6260154c0
Dilip <Dilip@DESKTOP-SVH4062>
2017-02-26 03:40:26 +0530

    spark kafka direct approach with zero data loss

--- src/main/scala/com/spark/KafkaDirectStream.scala
+++ src/main/scala/com/spark/KafkaDirectStream.scala
@@ -5,69 +5,57 @@ import org.apache.log4j.Level
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.Seconds
-import org.apache.spark.streaming.Minutes
-import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka._
 import kafka.serializer.StringDecoder
-import org.apache.spark.TaskContext
 import org.I0Itec.zkclient.ZkClient
 import kafka.common.TopicAndPartition
 import kafka.utils.ZkUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.InputDStream
 import kafka.message.MessageAndMetadata
-import org.apache.spark.streaming.Time
 
-object KafkaDirectStream extends App {
+object KafkaDirectStream {
 
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
 
-  println("Spark kafka Streaming...")
-  
-  val topicset = "dilip".split(",").toSet
+  def main(args: Array[String]): Unit = {
+    val topicset = "dilip".split(",").toSet
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> "localhost:9092",
+      "serializer.class" -> "kafka.serializer.StringEncoder", "group.id" -> "spark-kafka-consumer"
+    )
+    val checkpointDir = "D:\\tmp\\checkpointLogs"
 
-  val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092",
-    "zookeeper.connect" -> "localhost:2181", "group.id" -> "testgroup")
+    val ssc = StreamingContext.getOrCreate(checkpointDir, initilizeStreamingContext(topicset, kafkaParams, checkpointDir) _)
+    ssc.start()
+    ssc.awaitTermination()
 
-  val checkpointDir = "D:\\tmp\\checkpointLogs"
-  
-  val ssc =StreamingContext.getOrCreate(checkpointDir, setupSsc(topicset,kafkaParams,checkpointDir) _) 
-    
-
-  def setupSsc(topicset: Set[String], kafkaParams: Map[String, String],checkpointDir:String)(): StreamingContext = {
-    
+  }
+  def initilizeStreamingContext(topicset: Set[String], kafkaParams: Map[String, String], checkpointDir: String)(): StreamingContext = {
 
     val sparkConf = new SparkConf().setAppName("SparkKafkaStreamService").setMaster("local[*]")
-    
     val ssc = new StreamingContext(sparkConf, Seconds(5))
-
     //create direct kafka stream
     val messages = createCustomDirectKafkaStream(ssc, kafkaParams, "localhost:2181", "/kafka", topicset).map(_._2)
-    messages.foreachRDD((rdd: RDD[String], time: Time) => {
-
-      rdd.foreachPartition { partitionOfRecords =>
-        {
-          if (partitionOfRecords.isEmpty) {
-            println("partition of records empty ,IGNORING THIS PARTITION")
-          } else {
-            rdd.collect().foreach(println)
-          }
-        }
-
+    messages.foreachRDD(rdd => {
+      if (rdd.take(1).length == 0) {
+        println("Empty Record......")
+      } else {
+        rdd.collect().foreach(println)
       }
 
     })
+    //ssc.checkpoint(checkpointDir)
     ssc
   }
 
   //createDirectStream method overloaded
-
   def createCustomDirectKafkaStream(ssc: StreamingContext, kafkaParams: Map[String, String], zkHosts: String, zkPath: String, topics: Set[String]): InputDStream[(String, String)] = {
 
     val topic = topics.last
     val zkClient = new ZkClient(zkHosts, 30000, 30000)
-    println("zookeeper..."+zkClient)
+    println("zookeeper..." + zkClient)
     val storedOffsets = readOffsets(zkClient, zkHosts, zkPath, topic)
     val kafkaStream = storedOffsets match {
       case None => //start from the latest offsets
@@ -83,16 +71,15 @@ object KafkaDirectStream extends App {
   }
 
   //Read the previously saved offsets from zookeeper
-
   private def readOffsets(zkClient: ZkClient, zkHosts: String, zkPath: String, topic: String): Option[Map[TopicAndPartition, Long]] = {
-    println("Reading offsets from zookeeper....."+zkHosts+"   ,"+zkPath)
+    println("Reading offsets from zookeeper....." + zkHosts + "   ," + zkPath)
     val stopwatch = new Stopwatch()
-    val (offsetsRangesStrOpt, _) = ZkUtils.readDataMaybeNull(zkClient, zkHosts)
+    val (offsetsRangesStrOpt, _) = ZkUtils.readDataMaybeNull(zkClient, zkPath)
     offsetsRangesStrOpt match {
       case Some(offsetsRangerStr) =>
-        println("Read offset ranges..:"+offsetsRangerStr)
+        println("Read offset ranges..:" + offsetsRangerStr)
         val offsets = offsetsRangerStr.split(",")
-          .map(s => s.split(","))
+          .map(s => s.split(":"))
           .map { case Array(partitionStr, offsetStr) => (TopicAndPartition(topic, partitionStr.toInt) -> offsetStr.toLong) }
           .toMap
 
